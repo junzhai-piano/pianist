d67d47724de725f87944b279bee5fc0ac308bfec
bailebao3 <elizazhai@gmail.com>
2023-10-10 12:13:04 -0700

    fixed  a lot

--- shop_scripts.js
+++ shop_scripts.js
@@ -1,10 +1,14 @@
-// script.js
-let cart = [];
+let cart = {};
 let total = 0;
 
 function addToCart(productName, price) {
-  cart.push({ productName, price });
-  total += price;
+  if (cart[productName]) {
+    // Product already exists in the cart, increment quantity
+    cart[productName].quantity += 1;
+  } else {
+    // Product doesn't exist in the cart, add it with quantity 1
+    cart[productName] = { price: price, quantity: 1 };
+  }
 
   // Update the cart display
   updateCart();
@@ -15,11 +19,20 @@ function updateCart() {
   const totalElement = document.getElementById("total");
   cartList.innerHTML = "";
 
-  cart.forEach((item) => {
+  for (const productName in cart) {
+    const product = cart[productName];
     const listItem = document.createElement("li");
-    listItem.innerHTML = `${item.productName}: $${item.price.toFixed(2)}`;
+    const totalPrice = product.price * product.quantity;
+    listItem.innerHTML = `${productName} (Quantity: ${
+      product.quantity
+    }) - $${totalPrice.toFixed(2)}`;
     cartList.appendChild(listItem);
-  });
+  }
 
+  // Calculate the total price by summing up the prices of all products
+  total = Object.values(cart).reduce(
+    (acc, product) => acc + product.price * product.quantity,
+    0
+  );
   totalElement.textContent = total.toFixed(2);
 }
